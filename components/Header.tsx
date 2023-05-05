1aa50bafe2a04584fc542b93a727d15a0d0f94e3
Nemika-Haj <mmnemijus@gmail.com>
2023-05-05 22:52:47 +0300

    Added header logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,31 @@
+import { Flex, Heading, chakra } from "@chakra-ui/react";
+
 export default function Header() {
 
     return (
-        <>
-        </>
+        <Flex
+            border="2px solid red"
+            height="6em"
+            width="100vw"
+            wrap="wrap"
+            padding={5}
+        >
+            <Heading>
+                Made You Look
+                <chakra.span
+                    ml={3}
+                    color="lime"
+                    fontWeight="bold"
+                    fontSize="2xl"
+                    border="2px solid lime"
+                    px={2}
+                    rounded="md"
+                    _hover={{ background: "rgba(0, 240, 0, 0.15)" }}
+                    transition="background .1s linear"
+                >
+                    .life
+                </chakra.span>
+            </Heading>
+        </Flex>
     )
 }
